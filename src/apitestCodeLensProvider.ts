bed6775077b01b36de2f1fcf29d2be8e1051713a
Steve Hillier <stephenhillier@gmail.com>
2019-03-12 22:45:12 -0700

    almost working test running

--- src/apitestCodeLensProvider.ts
+++ src/apitestCodeLensProvider.ts
@@ -4,16 +4,44 @@ class ApitestCodeLensProvider implements CodeLensProvider {
   public provideCodeLenses(document: TextDocument): Promise<CodeLens[]> {
     let requests : CodeLens[] = [];
 
-    let top = new Range(0,0,0,0)
+    let lines: string[] = document.getText().split(/\r?\n/g);
 
-    let c: Command = {
-      command: 'apitest.request',
-      title: 'Run request'
+    let isRequestsBlock = false
+    let indent = 0
+    let isFirstMatch = true
+
+    for (let i = 0; i < lines.length; i++) {
+
+      if (lines[i].trim() === 'requests:') {
+        isRequestsBlock = true
+      }
+
+      if (isRequestsBlock) {
+        const re = /([\s-]+)name:\s*(.+)/
+        const matches = re.exec(lines[i])
+  
+        // we expect a valid matching request name to have 3 matches.
+        if (matches && matches.length >= 3) {
+          
+          if (isFirstMatch) {
+            indent = matches[1].length
+            isFirstMatch = false
+          }
+
+          if (matches[1].length === indent) {
+            let c: Command = {
+              arguments: [`-f ${document.fileName} -t "${matches[2]}"`],
+              command: 'apitest.request',
+              title: 'Run request'
+            }
+        
+            let codeLens = new CodeLens(new Range(i, 0, i, 0), c)
+        
+            requests.push(codeLens)
+          }
+        }
+      }
     }
-
-    let codeLens = new CodeLens(top, c)
-
-    requests.push(codeLens)
 
     return Promise.resolve(requests)
   }
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { commands, ExtensionContext, languages, window } from 'vscode';
+import { commands, ExtensionContext, languages, window, OutputChannel } from 'vscode';
 import ApitestCodeLensProvider from './apitestCodeLensProvider';
+import * as cp from 'child_process';
+
+
+let _channel: OutputChannel;
+function getOutputChannel(): OutputChannel {
+	if (!_channel) {
+		_channel = window.createOutputChannel('apitest output');
+	}
+	return _channel;
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,11 +20,21 @@ export function activate(context: ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let commandDisposable = commands.registerCommand('apitest.request', () => {
+	let commandDisposable = commands.registerCommand('apitest.request', (args: string) => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		window.showInformationMessage('Hello World!');
+		let decoded = ''
+		const childProcess = cp.spawn(`apitest ${args}`);
+		if (childProcess.pid) {
+			childProcess.stdout.on('data', (data: Buffer) => {
+				decoded += data;
+			});
+			childProcess.stdout.on('end', () => {
+				let channel = getOutputChannel();
+				channel.appendLine(decoded)
+				channel.show(true)
+			})
+		}
 	});
 
   let docSelector = {
